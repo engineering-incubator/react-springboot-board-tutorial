b78a9460ff47b5c23c4806e0fe4967975d38a4e5
martinyounghoonkim <hoons0131@gmail.com>
2022-02-05 22:34:38 +0900

    스프링 프로젝트 생성

---
+++ src/main/java/com/example/reactspringbootboardtutorial/ReactSpringbootBoardTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactspringbootboardtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringbootBoardTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringbootBoardTutorialApplication.class, args);
+	}
+
+}
