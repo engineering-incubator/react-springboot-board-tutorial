feb756b813b3a9dc901bce1beeb04e45fd476e97
Rita Amane <uuil31@gmail.com>
2022-03-03 21:36:41 +0900

    Article CRUD 완성 Response Wrapping 완성

--- src/main/java/com/example/reactspringbootboardtutorial/article/controller/ArticleController.java
+++ src/main/java/com/example/reactspringbootboardtutorial/article/controller/ArticleController.java
@@ -3,6 +3,8 @@ package com.example.reactspringbootboardtutorial.article.controller;
 import com.example.reactspringbootboardtutorial.article.dto.ArticleCreateDto;
 import com.example.reactspringbootboardtutorial.article.dto.ArticleDetailsDto;
 import com.example.reactspringbootboardtutorial.article.service.ArticleService;
+import com.example.reactspringbootboardtutorial.common.dto.WrappedResponseDto;
+import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,22 +24,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class ArticleController {
   private final ArticleService articleService;
 
+  @GetMapping
+  public WrappedResponseDto<List<ArticleDetailsDto>> getArticleList() {
+    return WrappedResponseDto.success(articleService.getArticleList());
+  }
+
   @GetMapping("/{articleId}")
-  public ArticleDetailsDto getArticle(@PathVariable Long articleId) {
-    return articleService.getArticle(articleId);
+  public WrappedResponseDto<ArticleDetailsDto> getArticle(@PathVariable Long articleId) {
+    return WrappedResponseDto.success(articleService.getArticle(articleId));
   }
 
   @PostMapping
-  public ArticleDetailsDto createArticle(@Valid @RequestBody ArticleCreateDto articleCreateDto) {
-    return articleService.saveArticle(articleCreateDto);
+  public WrappedResponseDto<ArticleDetailsDto> createArticle(@Valid @RequestBody ArticleCreateDto articleCreateDto) {
+    return WrappedResponseDto.success(articleService.saveArticle(articleCreateDto));
   }
 
   @DeleteMapping("/{articleId}")
   public void deleteArticle(@PathVariable Long articleId) {
-    try {
       articleService.deleteArticle(articleId);
-    } catch (Exception e) {
-      System.err.println(e.getMessage());
-    }
+  }
+
+  @PutMapping("/{articleId}")
+  public WrappedResponseDto<ArticleDetailsDto> updateArticle(@PathVariable Long articleId, @Valid @RequestBody ArticleCreateDto articleUpdateDto) {
+    return WrappedResponseDto.success(articleService.updateArticle(articleId, articleUpdateDto));
   }
 }
