f1265681743f70c903b63f606a0d76269fb746d7
yanwhocodes <ynawhocodes@gmail.com>
2022-03-16 10:20:05 +0900

    회원가입 정규식 추가

---
+++ front-end/src/validation/SignUpRegEx.jsx
@@ -0,0 +1,17 @@
+export const isPassword= (asValue) =>  {
+	var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/;
+ 
+	return regExp.test(asValue);
+}
+​
+export const isEmail = (asValue) =>{
+	var regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+ 
+	return regExp.test(asValue);
+}
+ 
+export const isPhoneNumber = (asValue) => {
+	var regExp = /^01(?:0|1|[6-9])-(?:\d{3}|\d{4})-\d{4}$/;
+ 
+	return regExp.test(asValue);
+}
