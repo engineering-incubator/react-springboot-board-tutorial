952a535ba0da699cefa2046c639973030dd9c615
Rita Amane <uuil31@gmail.com>
2022-03-30 19:09:17 +0900

    PR 지적사항 커밋

--- src/main/java/com/example/reactspringbootboardtutorial/authentication/controller/AuthenticationController.java
+++ src/main/java/com/example/reactspringbootboardtutorial/authentication/controller/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.example.reactspringbootboardtutorial.authentication.controller;
 
 import com.example.reactspringbootboardtutorial.authentication.dto.SignUpDto;
 import com.example.reactspringbootboardtutorial.authentication.dto.SignUpRequestDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/authentication")
+@RequiredArgsConstructor
 public class AuthenticationController {
+
 
     @PostMapping("/sign-up")
     public SignUpDto signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
