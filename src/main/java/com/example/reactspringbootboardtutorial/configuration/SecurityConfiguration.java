42040acbf7a77edb8bf5e4eff6003cff8f0258b4
ritaru <rita@rlta.dev>
2022-04-11 17:36:25 +0900

    Added sign-out feature

--- src/main/java/com/example/reactspringbootboardtutorial/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/reactspringbootboardtutorial/configuration/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.reactspringbootboardtutorial.configuration;
 
+import com.example.reactspringbootboardtutorial.authentication.CustomLogoutSuccessHandler;
 import com.example.reactspringbootboardtutorial.authentication.service.CustomUserDetailsService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +20,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   CustomUserDetailsService userDetailsService;
 
+  @Bean
+  public CustomLogoutSuccessHandler customLogoutSuccessHandler() {
+    return new CustomLogoutSuccessHandler();
+  }
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
@@ -33,7 +39,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
           .disable()
         .formLogin()
           .disable()
-        .logout();
+        .logout()
+            .logoutUrl("/v1/authentication/sign-out")
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID")
+            .logoutSuccessHandler(customLogoutSuccessHandler());
   }
 
   @Override
