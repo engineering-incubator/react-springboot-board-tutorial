1d5fece7c8fc81467018a343975cb410031dfb81
ritaru <rita@rlta.dev>
2022-04-11 16:59:42 +0900

    Removed author IP and replaced with username

--- src/main/java/com/example/reactspringbootboardtutorial/common/dto/PageableDto.java
+++ src/main/java/com/example/reactspringbootboardtutorial/common/dto/PageableDto.java
@@ -6,11 +6,11 @@ import java.util.List;
 
 @Getter
 public class PageableDto<T> {
-	private List<T> items;
-	private int totalPages;
-	private int currentPage;
-	private int size;
-	private Long totalElements;
+	private final List<T> items;
+	private final int totalPages;
+	private final int currentPage;
+	private final int size;
+	private final Long totalElements;
 
 	public PageableDto(List<T> items, int totalPages, int currentPage, int size, Long totalElements) {
 		this.items = items;
