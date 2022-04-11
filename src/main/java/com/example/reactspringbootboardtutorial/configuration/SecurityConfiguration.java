db0bc58ecfd2d1ca4764eff33599c6094419d625
ritaru <rita@rlta.dev>
2022-04-11 13:53:07 +0900

    Added user authentication service

--- src/main/java/com/example/reactspringbootboardtutorial/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/reactspringbootboardtutorial/configuration/SecurityConfiguration.java
@@ -1,29 +1,49 @@
 package com.example.reactspringbootboardtutorial.configuration;
 
+import com.example.reactspringbootboardtutorial.authentication.service.CustomUserDetailsService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+  CustomUserDetailsService userDetailsService;
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
         .authorizeRequests()
-          .anyRequest()
-            .permitAll()
+          .antMatchers("/v1/authentication/**")
+          .permitAll()
+          .anyRequest().authenticated()
         .and()
+        .csrf()
+          .disable()
         .cors()
           .disable()
         .formLogin()
-          .disable();
+          .disable()
+        .logout();
   }
 
   @Override
-  public void configure(WebSecurity web) {
-    web.ignoring().anyRequest();
+  protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+    authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(userDetailsService.passwordEncoder());
+  }
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
   }
 }
