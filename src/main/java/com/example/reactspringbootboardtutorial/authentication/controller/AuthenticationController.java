ca4eb7844dfb1463fd2c9283e9c429bba5852e90
ritaru <rita@rlta.dev>
2022-04-11 16:59:42 +0900

    Removed author IP and replaced with username

--- src/main/java/com/example/reactspringbootboardtutorial/authentication/controller/AuthenticationController.java
+++ src/main/java/com/example/reactspringbootboardtutorial/authentication/controller/AuthenticationController.java
@@ -1,23 +1,23 @@
 package com.example.reactspringbootboardtutorial.authentication.controller;
 
 import com.example.reactspringbootboardtutorial.authentication.dto.SignInRequestDto;
-import com.example.reactspringbootboardtutorial.authentication.dto.SignUpDto;
 import com.example.reactspringbootboardtutorial.authentication.dto.SignUpRequestDto;
 import com.example.reactspringbootboardtutorial.authentication.service.CustomUserDetailsService;
 import com.example.reactspringbootboardtutorial.common.dto.WrappedResponseDto;
-
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/v1/authentication")
@@ -40,11 +40,6 @@ public class AuthenticationController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
         session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
 
-        return WrappedResponseDto.success(SecurityContextHolder.getContext().getAuthentication());
-    }
-
-    @GetMapping
-    public WrappedResponseDto<?> checkCredentials() {
         return WrappedResponseDto.success(SecurityContextHolder.getContext().getAuthentication());
     }
 }
