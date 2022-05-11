003b9bffe540be46e13228aeef62f4e491ceff7d
Rita Amane <rita@rlta.dev>
2022-05-11 20:10:32 +0900

    2022-05-11 수정내용 반영

--- src/main/java/com/example/reactspringbootboardtutorial/authentication/filter/ExceptionHandlerFilter.java
+++ src/main/java/com/example/reactspringbootboardtutorial/authentication/filter/ExceptionHandlerFilter.java
@@ -1,22 +1,29 @@
 package com.example.reactspringbootboardtutorial.authentication.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.JwtException;
-import org.springframework.web.filter.OncePerRequestFilter;
-
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> errorObject = new HashMap<>();
+        errorObject.put("code", "FAILURE");
+
         try {
             filterChain.doFilter(request, response);
         } catch (JwtException jwtException) {
             response.setStatus(401);
-            response.getWriter().println("{\"code\": \"FAILURE\", \"message\": \"JWT token is invalid.\"}");
+            errorObject.put("message", jwtException.getMessage());
+            response.getWriter().println(mapper.writeValueAsString(errorObject));
         }
     }
 }
