b5e37820ce9dda65a2ae4cef023b4ba73339aee2
yanwhocodes <ynawhocodes@gmail.com>
2022-05-11 21:06:06 +0900

    [게시글] 게시글 생성 시 제목과 본문 없을 경우 api 안찌르고 프론트단에서 에러띄우도록 수정

--- front-end/src/screens/article/CreateArticle.jsx
+++ front-end/src/screens/article/CreateArticle.jsx
@@ -8,7 +8,8 @@ import ErrorMessage from '../../components/common/ErrorMessage';
 
 const CreateArticle = () => {
   const [article, setArticle] = useState({ title: '', content: '' });
-
+  const [isClickSubmitButton, setIsClickSubmitButton] = useState(false);
+ 
   const getValue = (e) => {
     setArticle({
       ...article,
@@ -41,7 +42,7 @@ const CreateArticle = () => {
           name="title"
           onChange={getValue}
         ></textarea>
-        {!article.title && ( isTitleFiveLettersLess(article.title.length) ? <ErrorMessage>❕ 글 제목을 5글자 이상으로 입력해주세요</ErrorMessage>:<ErrorMessage>❕ 글 제목을 입력해주세요</ErrorMessage>)}
+        {isClickSubmitButton && !article.title && <ErrorMessage>❕ 글 제목을 입력해주세요</ErrorMessage>}
         <textarea
           cols="60"
           rows="30"
@@ -49,11 +50,12 @@ const CreateArticle = () => {
           name="content"
           onChange={getValue}
         ></textarea>
-        {!article.content && <ErrorMessage>❕ 본문 내용을 입력해주세요</ErrorMessage>}
+        {isClickSubmitButton && !article.content && <ErrorMessage>❕ 본문 내용을 입력해주세요</ErrorMessage>}
 
         <Button
           onClick={() => {
-            isAllFilled(article) && isTitleFiveLettersLess(article.title.length) && requestCreate();
+            setIsClickSubmitButton(true)
+            isAllFilled(article) && !isTitleFiveLettersLess(article.title.length) && requestCreate();
           }}
         >
           작성 완료
